928511ac6871652d5808a29255374783ea8a340f
STEPHAN CHRISTOPHER HOLLY <stephan@axiomcoding.com>
2017-10-25 16:58:20 -0700

    fixed knexfile

--- knexfile.js
+++ knexfile.js
@@ -7,39 +7,23 @@ module.exports = {
     connection: {
       database: 'sportsdb',
       host: '127.0.0.1'
-    }
-  },
-
-  seeds: {
-    directory: './seeds'
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
+      directory: './migrations'
+    },
+  seeds: {
+  directory: './seeds'
+}},
 
   production: {
     client: 'pg',
-    connection: 'postgres://cdezepdikwknym:07e0d58c039124d2033018e4e1afa12ebae90b9ce35523a44db520594ddb96b4@ec2-54-225-88-191.compute-1.amazonaws.com:5432/de8t3h56mm1ss6',
-    pool: {
-      min: 2,
-      max: 10
-    },
+    connection: process.env.DATABASE_URL,
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
+      directory: './migrations'
+    },
+  seeds: {
+  directory: './seeds'
+}
+}
 
 };
